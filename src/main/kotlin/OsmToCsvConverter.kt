10ab8f890a8f9b5daa109e66b2974f4bdf0af2ef
Jonathan Locke <jon@thanlocke.com>
2025-02-24 21:03:19 -0700

    work on osm to csv converter

--- src/main/kotlin/OsmToCsvConverter.kt
+++ src/main/kotlin/OsmToCsvConverter.kt
@@ -1,34 +1,45 @@
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer
+import org.openstreetmap.osmosis.core.domain.v0_6.Entity
 import org.openstreetmap.osmosis.core.domain.v0_6.Node
+import org.openstreetmap.osmosis.core.domain.v0_6.Relation
 import org.openstreetmap.osmosis.core.domain.v0_6.Way
 import org.openstreetmap.osmosis.core.task.v0_6.Sink
 import org.openstreetmap.osmosis.pbf2.v0_6.PbfReader
 import java.io.File
 import java.io.FileWriter
+import kotlin.streams.toList
 
 class OsmToCsvConverter(private val inputFile: String)
 {
     private val tagWriter = FileWriter("tags.csv")
     private val nodeWriter = FileWriter("nodes.csv")
     private val wayWriter = FileWriter("ways.csv")
+    private val relationWriter = FileWriter("relations.csv")
 
     init
     {
         tagWriter.write("epochMillis,id,key,value\n")
-        nodeWriter.write("epochMillis,id,latitude,longitude\n")
-        wayWriter.write("epochMillis,id,nodeId,latitude,longitude\n")
+        nodeWriter.write("epochMillis,id,version,changeset,user,uid,lat,lon\n")
+        wayWriter.write("epochMillis,id,version,changeset,user,uid,geometry\n")
+        relationWriter.write("epochMillis,id,version,changeset,user,uid,members\n")
+    }
+
+    fun entityColumns(entity: Entity): String
+    {
+        return "${entity.timestamp.time},${entity.id},${entity.version},${entity.changesetId},${entity.user.name},${entity.user.id}"
     }
 
     fun convert()
     {
         val reader = PbfReader(File(inputFile), 4)
-        val latitudes = HashMap<Long, Double>();
-        val longitudes = HashMap<Long, Double>();
+        val latitudes = HashMap<Long, Double>()
+        val longitudes = HashMap<Long, Double>()
+
         reader.setSink(object : Sink
         {
             override fun process(entityContainer: EntityContainer)
             {
-                val it = entityContainer.entity;
+                val it = entityContainer.entity
                 val timeAndId = "${it.timestamp.time},${it.id}"
 
                 for (tag in it.tags)
@@ -40,7 +51,7 @@ class OsmToCsvConverter(private val inputFile: String)
                 {
                     is Node ->
                     {
-                        nodeWriter.write("${timeAndId},${it.latitude},${it.longitude}\n")
+                        nodeWriter.write("${entityColumns(it)},${it.latitude},${it.longitude}\n")
                         latitudes[it.id] = it.latitude
                         longitudes[it.id] = it.longitude
                     }
@@ -49,8 +60,13 @@ class OsmToCsvConverter(private val inputFile: String)
                     {
                         for (node in it.wayNodes)
                         {
-                            wayWriter.write("${timeAndId},${node.nodeId},${latitudes[node.nodeId]},${longitudes[node.nodeId]}\n")
+                            val geometry = Wkt.convertToWkt(it.wayNodes.stream().map { LatLon(it.latitude, it.longitude) }.toList())
+                            nodeWriter.write("${entityColumns(it)},${geometry}\n")
                         }
+                    }
+
+                    is Relation ->
+                    {
                     }
                 }
             }
@@ -64,6 +80,7 @@ class OsmToCsvConverter(private val inputFile: String)
                 tagWriter.close()
                 nodeWriter.close()
                 wayWriter.close()
+                relationWriter.close()
             }
 
             override fun close()
@@ -75,7 +92,7 @@ class OsmToCsvConverter(private val inputFile: String)
 
     private fun quote(value: String): String
     {
-        return value.replace("\"", "\\\"");
+        return value.replace("\"", "\\\"")
     }
 }
 
