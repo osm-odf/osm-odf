5c8bcc1466556582fc0fd4504b568fe38d2b8930
Martijn van Exel <m@rtijn.org>
2025-03-12 15:35:48 -0600

    work

---
+++ TODO.md
@@ -0,0 +1,7 @@
+TODO
+
+- Bounding box around Berlin for minutely updates
+- Kamu pull ?!??!
+- Dockerize
+- S3 bucket
+- What would a minimal demo look like?
--- script/fetch_and_pull.sh
+++ script/fetch_and_pull.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# before you run this, you gotta go into osm-minutely-changes, run 
+# python -m venv .venv
+# pip install -r requirements.txt
+# source .venv/bin/activate
+
 source ../osm-minutely-changes/.venv/bin/activate
 python ../osm-minutely-changes/consumer.py
 
